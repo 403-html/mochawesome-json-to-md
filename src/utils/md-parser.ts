5c224957f588d3db0e1ec3692f6d2b43b80f6cd1
403-html <tymoteusz.stepien@wp.pl>
2022-12-18 20:51:42 +0100

    feat: cleanup the method

--- src/utils/md-parser.ts
+++ src/utils/md-parser.ts
@@ -1,4 +1,48 @@
-export const generateMarkdown = (obj: object, template: string): string => {
+export const generateMarkdown = (
+  obj: object,
+  template: string,
+  blockRegex?: RegExp,
+  singleRegex?: RegExp
+): string => {
+  // Cache the regular expressions if they were not provided
+  const blockRegexCache = blockRegex || /\{\{(.*?)\}\}([\s\S]*?)\{\{\/\1\}\}/g;
+  const singleRegexCache = singleRegex || /\{\{(.*?)\}\}/g;
+
+  // Extract the logic for replacing block tags and single tags into separate functions
+  const replaceBlockTags = (
+    _match: string,
+    key: string,
+    nestedTemplate: string
+  ) => {
+    if (obj.hasOwnProperty(key) && Array.isArray(obj[key])) {
+      return obj[key]
+        .map((item) => {
+          return generateMarkdown(
+            item,
+            nestedTemplate.trim(),
+            blockRegexCache,
+            singleRegexCache
+          );
+        })
+        .join("\n");
+    } else if (obj.hasOwnProperty(key) && typeof obj[key] === "object") {
+      return generateMarkdown(
+        obj[key],
+        nestedTemplate.trim(),
+        blockRegexCache,
+        singleRegexCache
+      );
+    }
+  };
+  const replaceSingleTags = (_match: string, key: string) => {
+    if (
+      obj.hasOwnProperty(key) &&
+      !(typeof obj[key] === "object" || Array.isArray(obj[key]))
+    ) {
+      return obj[key];
+    }
+  };
+
   const isArrayAndEmpty = Array.isArray(obj) && obj.length === 0;
   const isObjAndEmpty =
     typeof obj === "object" && Object.keys(obj).length === 0;
@@ -8,30 +52,11 @@ export const generateMarkdown = (obj: object, template: string): string => {
     return template;
   }
 
-  // Replace block of tags
-  const regex = /\{\{(.*?)\}\}([\s\S]*?)\{\{\/\1\}\}/g;
-  template = template.replace(regex, (_match, key, nestedTemplate) => {
-    if (obj.hasOwnProperty(key) && Array.isArray(obj[key])) {
-      return obj[key]
-        .map((item) => {
-          return generateMarkdown(item, nestedTemplate.trim());
-        })
-        .join("\n");
-    } else if (obj.hasOwnProperty(key) && typeof obj[key] === "object") {
-      return generateMarkdown(obj[key], nestedTemplate.trim());
-    }
-  });
+  // Replace block tags
+  template = template.replace(blockRegexCache, replaceBlockTags);
 
   // Replace single tags
-  const regex2 = /\{\{(.*?)\}\}/g;
-  template = template.replace(regex2, (_match, key) => {
-    if (
-      obj.hasOwnProperty(key) &&
-      !(typeof obj[key] === "object" || Array.isArray(obj[key]))
-    ) {
-      return obj[key];
-    }
-  });
+  template = template.replace(singleRegexCache, replaceSingleTags);
 
   return template;
 };
